3932c54dbdd45b65194aa8d9910ffce9cafb10fd
mah077 <mr_m.hijazi@hotmail.com>
2019-07-12 16:31:19 -0400

    composite key and mapping

--- festivalApp/src/main/java/com/revature/festivalapp/pojos/Schedule.java
+++ festivalApp/src/main/java/com/revature/festivalapp/pojos/Schedule.java
@@ -2,34 +2,63 @@ package com.revature.festivalapp.pojos;
 
 import java.time.LocalDateTime;
 
+import javax.persistence.AttributeOverride;
+
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.IdClass;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+
+
+@Entity
+//name of the table  in DB
+@Table(name="schedule")
+@IdClass(Stage.class)
+
 public class Schedule {
-	private Integer stageNumber;
-	private LocalDateTime startTime;
+//reference for composite key
+//https://www.concretepage.com/hibernate/example-embeddedid-hibernate
+	
+	// we should create sequence in the database table start_time_sequence
+	@SequenceGenerator(name="start_time_sequence", sequenceName="start_time_sequence")
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="")
+	@EmbeddedId
+	//in AttributeOverride we combine the two keys as a composite key 
+	@AttributeOverride(name="stage_number", column=@Column(name="start_time"))
+	private ScheduleEmbedded se;
+	
+	@Column(name="end_time")
 	private LocalDateTime endTime;
+	
+	@Column(name="artist_name")
 	private String crewEmail;
+		 	
 	public Schedule() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
-	public Schedule(Integer stageNumber, LocalDateTime startTime, LocalDateTime endTime, String crewEmail) {
-		super();
-		this.stageNumber = stageNumber;
-		this.startTime = startTime;
+	// inside the scheduleEmbedded we have the composite key			
+	public Schedule(ScheduleEmbedded se, LocalDateTime endTime, String crewEmail) {
+
+		this.se=se;
+		
 		this.endTime = endTime;
 		this.crewEmail = crewEmail;
 	}
-	public Integer getStageNumber() {
-		return stageNumber;
+
+	
+	public ScheduleEmbedded getSe() {
+		return se;
 	}
-	public void setStageNumber(Integer stageNumber) {
-		this.stageNumber = stageNumber;
+	public void setSe(ScheduleEmbedded se) {
+		this.se = se;
 	}
-	public LocalDateTime getStartTime() {
-		return startTime;
-	}
-	public void setStartTime(LocalDateTime startTime) {
-		this.startTime = startTime;
-	}
+	
+	
 	public LocalDateTime getEndTime() {
 		return endTime;
 	}
