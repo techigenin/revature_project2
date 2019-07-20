40a40af7cf18463ce687926005db285a91b6699d
znoori455 <znoori455@gmail.com>
2019-07-20 12:26:22 -0400

    src folder replaced

--- Festival-Angular/src/app/app.component.spec.ts
+++ Festival-Angular/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Festival-Angular'`, () => {
+  it(`should have as title 'Festival-App'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Festival-Angular');
+    expect(app.title).toEqual('Festival-App');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Festival-Angular!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Festival-App!');
   });
 });
