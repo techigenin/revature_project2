1755afd04ae16aa254a195d6182470384e2395cb
Tevin <tevin.thomas62@yahoo.com>
2019-07-22 20:48:43 -0400

    no-session message

--- festivalApp/src/main/java/com/revature/festivalapp/controller/HomeController.java
+++ festivalApp/src/main/java/com/revature/festivalapp/controller/HomeController.java
@@ -2,14 +2,21 @@ package com.revature.festivalapp.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.festivalapp.pojos.FestivalEvent;
 import com.revature.festivalapp.services.FestivalEventServicesImpl;
 
+@RestController("/home")
 public class HomeController {
 	
 	private FestivalEventServicesImpl eventService;
@@ -19,7 +26,7 @@ public class HomeController {
 		this.eventService = eventService;
 	}
 	
-	@PostMapping("/home")
+	@GetMapping(value="/home")
 	public List<FestivalEvent> getAllFestivalEvents() {
 		return eventService.viewAllEvents();
 	}
