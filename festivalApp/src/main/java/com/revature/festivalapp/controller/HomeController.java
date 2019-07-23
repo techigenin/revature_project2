6a8512c9c342d55eeb1bb02ce6c3f08a249215d7
Tevin <tevin.thomas62@yahoo.com>
2019-07-23 19:08:47 -0400

    Working on Schedule

---
+++ festivalApp/src/main/java/com/revature/festivalapp/controller/HomeController.java
@@ -0,0 +1,48 @@
+package com.revature.festivalapp.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.festivalapp.pojos.FestivalEvent;
+import com.revature.festivalapp.services.FestivalEventServicesImpl;
+
+@RestController("/home")
+//@Controller
+public class HomeController {
+	
+	private FestivalEventServicesImpl eventService;
+	
+	@Autowired
+	public void setFestivalEventService(FestivalEventServicesImpl eventService ) {
+		this.eventService = eventService;
+	}
+	
+	@GetMapping//(value="/home")
+	public @ResponseBody String getAllFestivalEvents() {
+		ObjectMapper om = new ObjectMapper();
+		List<FestivalEvent> feList = eventService.viewAllEvents();
+		
+		try {
+			return om.writeValueAsString(feList);
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	
+	}
+
+}
+
---
+++ festivalApp/src/main/java/com/revature/festivalapp/controller/ManageRoleController.java
@@ -0,0 +1,12 @@
+package com.revature.festivalapp.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ManageRoleController {
+	
+	
+	
+	
+
+}
