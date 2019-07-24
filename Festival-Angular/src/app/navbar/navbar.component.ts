b50e905da3d76e1f6ae75101e56a938900fbe314
znoori455 <znoori455@gmail.com>
2019-07-23 20:14:41 -0400

    fixed property not found errors when running ng build --prod

--- Festival-Angular/src/app/navbar/navbar.component.ts
+++ Festival-Angular/src/app/navbar/navbar.component.ts
@@ -5,7 +5,10 @@ import { AssignedEvent } from '../shared/assigned-event.model';
 import { Router } from '@angular/router';
 import { map } from 'rxjs/operators';
 
-
+  // assignedEvents: AssignedEvent[] = [
+  //   new AssignedEvent('Lollapalooza', 'Manager', '8/7/19', '8/10/19'),
+  //   new AssignedEvent('Bonnaroo', 'Artist', '11/7/19', '12/10/19')
+  // ];
 
 @Component({
   selector: 'app-navbar',
@@ -14,12 +17,16 @@ import { map } from 'rxjs/operators';
 })
 export class NavbarComponent implements OnInit {
   loggedIn = false;
-  assignedEvents: AssignedEvent[] = [
-    new AssignedEvent('Lollapalooza', 'Manager', '8/7/19', '8/10/19'),
-    new AssignedEvent('Bonnaroo', 'Artist', '11/7/19', '12/10/19')
-  ];
+  // private assignedEvent:AssignedEvent;
+  // private assignedEvents: AssignedEvent[] = [
+  //   new AssignedEvent('Lollapalooza', 'Manager', '8/7/19', '8/10/19'),
+  //   new AssignedEvent('Bonnaroo', 'Artist', '11/7/19', '12/10/19')
+  // ];
+  assignedEvents: AssignedEvent[];
 
-  constructor(private http: HttpClient, private router: Router) {}
+  constructor(private http: HttpClient, private router: Router ) {
+
+  }
 
   // angular http client will take our object (data) and automatically convert to JSON data
   // must subscribe to request, otherwise angular assumes no one is interested in response and
@@ -27,7 +34,7 @@ export class NavbarComponent implements OnInit {
   // don't need to unsubscribe for observables provided by angular 
   onClickSubmit(data: { username: string; password: string }) {
     // sending http request
-    this.http.post('url', data).subscribe(responseData => {
+    this.http.post('/login', data).subscribe(responseData => {
       // angular will give you the response body 
       // if response is false, alert user invalid login
       // bool: Boolean = responseData;
@@ -48,7 +55,7 @@ export class NavbarComponent implements OnInit {
 
  private fetchUserEvents() {
    //     angle brackets<> tell ts what kind of object you're getting back; avoids Type errors
-   this.http.get<AssignedEvent>('getusereventsurl').pipe(map(responseData => {
+   this.http.get<AssignedEvent>('/').pipe(map(responseData => {
      const postsArray: AssignedEvent[] = [];
      for (const key in responseData) {
        postsArray.push({ ...responseData[key]});
@@ -62,5 +69,13 @@ export class NavbarComponent implements OnInit {
 
   ngOnInit() {
     const quickviews = bulmaQuickview.attach();
+    //const assignedEvents: AssignedEvent[];
+    this.assignedEvents = [
+      new AssignedEvent('Lollapalooza', 'Manager', '8/7/19', '8/10/19'),
+      new AssignedEvent('Bonnaroo', 'Artist', '11/7/19', '12/10/19')
+    ];
+
+
+
   }
 }
