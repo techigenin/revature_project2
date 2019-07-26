c9a0ddfc22007f4693c5b4029aa6a0a7befd0914
znoori455 <znoori455@gmail.com>
2019-07-26 14:01:51 -0400

    Last minute design

--- Festival-Angular/src/app/home/home.component.ts
+++ Festival-Angular/src/app/home/home.component.ts
@@ -6,6 +6,7 @@ import { AssignedEvent } from '../shared/assigned-event.model';
 
 import bulmaCarousel from '../../../node_modules/bulma-carousel/dist/js/bulma-carousel.min.js';
 import bulmaQuickview from '../../../node_modules/bulma-quickview/dist/js/bulma-quickview.js';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -18,19 +19,21 @@ export class HomeComponent implements OnInit {
 
   events: Event[] = [
     new Event(1, 2, 'Lollapalooza', 'Tampa, Fl', '8/7/19', '8/10/19', 'consectetur adipisicing elit. Dolorum sapiente explicabo, corporis totam! Labore reprehenderit beatae'),
-    new Event(1, 2, 'Bonnaroo', 'Zimbabwe', '11/7/19', '12/10/19', 'quas accusantium perferendis sapiente explicabo, corporis totam! Labore reprehenderit beatae magnam animi!'),
+    new Event(1, 2, 'Moonrise', 'Baltimore, MD', '11/7/19', '12/10/19', 'quas accusantium perferendis sapiente explicabo, corporis totam! Labore reprehenderit beatae magnam animi!'),
     new Event(1, 2, 'Camp Bisco', 'Philadelphia', '10/30/19', '11/13/14', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum consequatur numquam aliquam tenetur ad amet inventore hic beatae.')
   ];
 
 
-  onSelectEvent(selectedEvent: HTMLCollection) {
-    console.log(selectedEvent[0].firstChild.firstChild.textContent);
-    console.log(selectedEvent[0].children[2].firstChild.textContent);
-    console.log(selectedEvent[0].children[3].firstChild.textContent);
-    console.log(selectedEvent[0].children[3].lastChild.textContent);
+  onSelectEvent(selectedEvent: Event) {
+    console.log(selectedEvent);
+    this.router.navigate(['/schedule']);
+    // console.log(selectedEvent[0].firstChild.firstChild.textContent);
+    // console.log(selectedEvent[0].children[2].firstChild.textContent);
+    // console.log(selectedEvent[0].children[3].firstChild.textContent);
+    // console.log(selectedEvent[0].children[3].lastChild.textContent);
 
   }
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
 
 
--- Festival-Angular/src/app/stage-schedule/stage-schedule.component.ts
+++ Festival-Angular/src/app/stage-schedule/stage-schedule.component.ts
@@ -4,8 +4,9 @@ import { Stage } from '../shared/stage.model';
 import { EventDay } from '../shared/day.model';
 
 
-let d = new Date();
-d.setDate(24);
+// let d = new Date();
+// let a = new Date();
+// d.setDate(24);
 
 
 @Component({
@@ -20,6 +21,8 @@ export class StageScheduleComponent implements OnInit {
   perfNumber = 0;
   columnSize: number;
   eventDays: EventDay[];
+  d: Date;
+  a: Date;
   // eventDays: EventDay[] = [
   //   new EventDay(new Date(), [
   //     new Stage('Stage One', [
@@ -158,69 +161,72 @@ export class StageScheduleComponent implements OnInit {
     // }
     //console.log(size);
     console.log(this.columnSize);
+    this.d = new Date();
+    this.a = new Date();
+    this.a.setDate(25)
     this.eventDays = [
-      new EventDay(new Date(), [
-        new Stage('Stage One', [
-          new Performance('5:00 PM', '6:45 PM', 'Sublime', this.perfNumber++),
-          new Performance('6:45 PM', '8:00 PM', 'Zeds Dead', this.perfNumber++),
-          new Performance('8:00 PM', '9:20 PM', 'Excision', this.perfNumber++),
-          new Performance('9:20 PM', '10:30 PM', 'Big Gigantic', this.perfNumber++),
-          new Performance('10:30 PM', '12:00 AM', 'Jay Z', this.perfNumber++),
-          new Performance('12:00 AM', '1:00 AM', 'Kendrick Lamar', this.perfNumber++),
-          new Performance('1:00 AM', '2:45 AM', 'Hippie Sabotage', this.perfNumber++),
-          new Performance('2:45 AM', '4:00 AM', 'Griz', this.perfNumber++)
+      new EventDay(this.a, [
+        new Stage('Skull Candy Stage', [
+          new Performance('5:00 PM', '6:45 PM', 'Tsunami Bomb', this.perfNumber++),
+          new Performance('6:45 PM', '8:00 PM', 'In This Moment', this.perfNumber++),
+          new Performance('8:00 PM', '9:20 PM', 'The Dollyrots', this.perfNumber++),
+          new Performance('9:20 PM', '10:30 PM', 'Garbage', this.perfNumber++),
+          new Performance('10:30 PM', '12:00 AM', 'Elastica', this.perfNumber++),
+          new Performance('12:00 AM', '1:00 AM', 'Otep', this.perfNumber++),
+          new Performance('1:00 AM', '2:45 AM', 'Amon Amarth', this.perfNumber++),
+          new Performance('2:45 AM', '4:00 AM', 'Bad Religion', this.perfNumber++)
         ]),
-        new Stage('Stage Two', [
-          new Performance('5:00 PM', '6:45 PM', 'Sublime', this.perfNumber++),
-          new Performance('6:45 PM', '8:00 PM', 'Zeds Dead', this.perfNumber++),
-          new Performance('8:00 PM', '9:20 PM', 'Excision', this.perfNumber++),
-          new Performance('9:20 PM', '10:30 PM', 'Big Gigantic', this.perfNumber++),
-          new Performance('10:30 PM', '12:00 AM', 'Jay Z', this.perfNumber++),
-          new Performance('12:00 AM', '1:00 AM', 'Kendrick Lamar', this.perfNumber++),
-          new Performance('1:00 AM', '2:45 AM', 'Hippie Sabotage', this.perfNumber++),
-          new Performance('2:45 AM', '4:00 AM', 'Griz', this.perfNumber++)
+        new Stage('Matchstick', [
+          new Performance('5:00 PM', '6:45 PM', 'Kendrick Lamar', this.perfNumber++),
+          new Performance('6:45 PM', '8:00 PM', 'Odesza', this.perfNumber++),
+          new Performance('8:00 PM', '9:20 PM', 'Big Grizmatik', this.perfNumber++),
+          new Performance('9:20 PM', '10:30 PM', 'Troyboi', this.perfNumber++),
+          new Performance('10:30 PM', '12:00 AM', 'Excision', this.perfNumber++),
+          new Performance('12:00 AM', '1:00 AM', 'RL Grime', this.perfNumber++),
+          new Performance('1:00 AM', '2:45 AM', 'Zeds Dead', this.perfNumber++),
+          new Performance('2:45 AM', '4:00 AM', 'Hippie Sabotage', this.perfNumber++)
         ]),
-        new Stage('Stage Three', [
-          new Performance('5:00 PM', '6:45 PM', 'Sublime', this.perfNumber++),
-          new Performance('6:45 PM', '8:00 PM', 'Zeds Dead', this.perfNumber++),
-          new Performance('8:00 PM', '9:20 PM', 'Excision', this.perfNumber++),
-          new Performance('9:20 PM', '10:30 PM', 'Big Gigantic', this.perfNumber++),
-          new Performance('10:30 PM', '12:00 AM', 'Jay Z', this.perfNumber++),
-          new Performance('12:00 AM', '1:00 AM', 'Kendrick Lamar', this.perfNumber++),
-          new Performance('1:00 AM', '2:45 AM', 'Hippie Sabotage', this.perfNumber++),
-          new Performance('2:45 AM', '4:00 AM', 'Griz', this.perfNumber++)
+        new Stage('Clash', [
+          new Performance('5:00 PM', '6:45 PM', ' Lacuna Coil', this.perfNumber++),
+          new Performance('6:45 PM', '8:00 PM', 'Scarlxrd', this.perfNumber++),
+          new Performance('8:00 PM', '9:20 PM', 'Alice In Chains', this.perfNumber++),
+          new Performance('9:20 PM', '10:30 PM', 'Tech N9ne', this.perfNumber++),
+          new Performance('10:30 PM', '12:00 AM', 'Slipknot', this.perfNumber++),
+          new Performance('12:00 AM', '1:00 AM', 'Sevendust', this.perfNumber++),
+          new Performance('1:00 AM', '2:45 AM', 'Flume', this.perfNumber++),
+          new Performance('2:45 AM', '4:00 AM', 'SOPHIE', this.perfNumber++)
         ]),
       ]),
-      new EventDay(d, [
-        new Stage('Stage 1', [
-          new Performance('5:00 PM', '6:45 PM', 'Sublime', this.perfNumber++),
-          new Performance('6:45 PM', '8:00 PM', 'Zeds Dead', this.perfNumber++),
-          new Performance('8:00 PM', '9:20 PM', 'Excision', this.perfNumber++),
-          new Performance('9:20 PM', '10:30 PM', 'Big Gigantic', this.perfNumber++),
-          new Performance('10:30 PM', '12:00 AM', 'Jay Z', this.perfNumber++),
-          new Performance('12:00 AM', '1:00 AM', 'Kendrick Lamar', this.perfNumber++),
-          new Performance('1:00 AM', '2:45 AM', 'Hippie Sabotage', this.perfNumber++),
-          new Performance('2:45 AM', '4:00 AM', 'Griz', this.perfNumber++)
+      new EventDay(new Date(), [
+        new Stage('Skull Candy Stage', [
+          new Performance('5:00 PM', '6:45 PM', 'Tsunami Bomb', this.perfNumber++),
+          new Performance('6:45 PM', '8:00 PM', 'In This Moment', this.perfNumber++),
+          new Performance('8:00 PM', '9:20 PM', 'The Dollyrots', this.perfNumber++),
+          new Performance('9:20 PM', '10:30 PM', 'Garbage', this.perfNumber++),
+          new Performance('10:30 PM', '12:00 AM', 'Elastica', this.perfNumber++),
+          new Performance('12:00 AM', '1:00 AM', 'Otep', this.perfNumber++),
+          new Performance('1:00 AM', '2:45 AM', 'Amon Amarth', this.perfNumber++),
+          new Performance('2:45 AM', '4:00 AM', 'Bad Religion', this.perfNumber++)
         ]),
-        new Stage('Stage 2', [
-          new Performance('5:00 PM', '6:45 PM', 'Sublime', this.perfNumber++),
-          new Performance('6:45 PM', '8:00 PM', 'Zeds Dead', this.perfNumber++),
-          new Performance('8:00 PM', '9:20 PM', 'Excision', this.perfNumber++),
-          new Performance('9:20 PM', '10:30 PM', 'Big Gigantic', this.perfNumber++),
-          new Performance('10:30 PM', '12:00 AM', 'Jay Z', this.perfNumber++),
-          new Performance('12:00 AM', '1:00 AM', 'Kendrick Lamar', this.perfNumber++),
-          new Performance('1:00 AM', '2:45 AM', 'Hippie Sabotage', this.perfNumber++),
-          new Performance('2:45 AM', '4:00 AM', 'Griz', this.perfNumber++)
+        new Stage('No John Raw', [
+          new Performance('5:00 PM', '6:45 PM', 'Kendrick Lamar', this.perfNumber++),
+          new Performance('6:45 PM', '8:00 PM', 'Odesza', this.perfNumber++),
+          new Performance('8:00 PM', '9:20 PM', 'Big Grizmatik', this.perfNumber++),
+          new Performance('9:20 PM', '10:30 PM', 'Troyboi', this.perfNumber++),
+          new Performance('10:30 PM', '12:00 AM', 'Excision', this.perfNumber++),
+          new Performance('12:00 AM', '1:00 AM', 'RL Grime', this.perfNumber++),
+          new Performance('1:00 AM', '2:45 AM', 'Zeds Dead', this.perfNumber++),
+          new Performance('2:45 AM', '4:00 AM', 'Hippie Sabotage', this.perfNumber++)
         ]),
-        new Stage('Stage 3', [
-          new Performance('5:00 PM', '6:45 PM', 'Sublime', this.perfNumber++),
-          new Performance('6:45 PM', '8:00 PM', 'Zeds Dead', this.perfNumber++),
-          new Performance('8:00 PM', '9:20 PM', 'Excision', this.perfNumber++),
-          new Performance('9:20 PM', '10:30 PM', 'Big Gigantic', this.perfNumber++),
-          new Performance('10:30 PM', '12:00 AM', 'Jay Z', this.perfNumber++),
-          new Performance('12:00 AM', '1:00 AM', 'Kendrick Lamar', this.perfNumber++),
-          new Performance('1:00 AM', '2:45 AM', 'Hippie Sabotage', this.perfNumber++),
-          new Performance('2:45 AM', '4:00 AM', 'Griz', this.perfNumber++)
+        new Stage('Clash', [
+          new Performance('5:00 PM', '6:45 PM', ' Lacuna Coil', this.perfNumber++),
+          new Performance('6:45 PM', '8:00 PM', 'Scarlxrd', this.perfNumber++),
+          new Performance('8:00 PM', '9:20 PM', 'Alice In Chains', this.perfNumber++),
+          new Performance('9:20 PM', '10:30 PM', 'Tech N9ne', this.perfNumber++),
+          new Performance('10:30 PM', '12:00 AM', 'Slipknot', this.perfNumber++),
+          new Performance('12:00 AM', '1:00 AM', 'Sevendust', this.perfNumber++),
+          new Performance('1:00 AM', '2:45 AM', 'Flume', this.perfNumber++),
+          new Performance('2:45 AM', '4:00 AM', 'SOPHIE', this.perfNumber++)
         ]),
       ])
     ];
