c55a260f1018757b474d578a7c71d344e61df9a2
Mike James <techiegenin@gmail.com>
2019-07-26 01:39:33 -0400

    adding Schedule and Stage

---
+++ festivalApp/src/main/java/com/revature/festivalapp/controller/ManageScheduleController.java
@@ -0,0 +1,49 @@
+package com.revature.festivalapp.controller;
+
+
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.revature.festivalapp.pojos.Schedule;
+import com.revature.festivalapp.pojos.User;
+import com.revature.festivalapp.services.ScheduleServices;
+
+
+public class ManageScheduleController {
+			
+	ScheduleServices scheduleServices;
+	
+
+
+	@Autowired
+	public void setScheduleServices(ScheduleServices scheduleServices) {
+		this.scheduleServices = scheduleServices;
+	}
+
+
+
+	
+	@PostMapping(value="/schedule" ,consumes= {"application/json"})
+	public @ResponseBody boolean SaveOrUpdateStagePost(@RequestBody Schedule schedule, HttpSession sess) {
+
+		
+		User user =(User) sess.getAttribute("users");
+		
+		if (user != null) {
+		
+		scheduleServices.SaveOrUpdate(schedule);
+		return true;
+		
+		}
+
+		return false;
+	}
+	
+	
+	
+}
